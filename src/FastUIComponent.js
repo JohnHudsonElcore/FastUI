9983a968670eb7690c95998fabb7b2b5444f443d
JohnHudsonElcore <106264566+JohnHudsonElcore@users.noreply.github.com>
2022-05-25 19:43:17 +0100

    FastUIComponent.js

---
+++ src/FastUIComponent.js
@@ -0,0 +1,55 @@
+class UIComponent
+{
+  /**
+  * @param {Object} - Destructed
+  */
+	constructor({ domElement })
+	{
+		this.domElement = domElement;
+
+		this.state = {
+
+		};
+		this.update();
+	}
+  /**
+  * @param {Object} params - state properties to assign
+                           - similar to React, components have states, 
+                           - Only the component can modify its state as
+                           - the creation of a component doesn't expose
+                           - its class to make calls
+  * @returns - void
+  */
+	setState(params){
+		for(let prop in params){
+			this.state[prop] = params[prop];
+		}
+		this.update();
+	}
+  /**
+  * @param {String} key - the key for the state object
+  * @returns {Mixed}
+  */
+	getState(key)
+	{
+		return this.state[key];
+	}
+  /**
+  * @description - called by the system, 
+  * never gets called by anything else other than the 
+  * component internally
+  */
+	update()
+	{
+		this.domElement.innerHTML = this.render();
+	}
+  /**
+  * @abstract
+  * @returns {String}
+  * @usage - returns a HTML string to update the component's innerHTML
+  */
+	render(){
+		throw new Error("FastUI.Component Exception: render is an abstract method that needs implementing");
+	}
+}
+FastUI.Component = UIComponent;
